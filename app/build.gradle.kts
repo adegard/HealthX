46d78e64cb08d273ba10263ff772c625a40e1384
adegard <degardinarnaud@gmail.com>
2026-08-15 13:16:46 +0200

    Bump compileSdk to 35 for sensor foreground service type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.healthmonitor"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.healthmonitor"
